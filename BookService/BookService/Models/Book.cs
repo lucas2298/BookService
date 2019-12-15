0e1922174cfcf34a4fe9540b45d52c4065a18abe
lucas2298 <tienthang2298@gmail.com>
2019-12-15 22:32:17 +0700

    New commit

---
+++ BookService/BookService/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookService.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Tile { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+        // Foregin Key
+        public int AuthorId { get; set; }
+        // Navigation property
+        public Author Author { get; set; }
+    }
+}
